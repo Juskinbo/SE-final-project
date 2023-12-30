8c293b5866805fdb965aacd51a18811493069030
Juskinbo <juskinbo@outlook.com>
2023-12-31 00:53:32 +0800

    init PostComponent

---
+++ src/components/PostComponent/PostComponent.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const PostComponent = () => {
+  return (
+    <Grid>
+      <Grid.Item text='文字'>
+        <Image />
+      </Grid.Item>
+      <Grid.Item text='文字'>
+        <Image />
+      </Grid.Item>
+      <Grid.Item text='文字'>
+        <Image />
+      </Grid.Item>
+      <Grid.Item text='文字'>
+        <Image />
+      </Grid.Item>
+      <Grid.Item text='文字'>
+        <Image />
+      </Grid.Item>
+      <Grid.Item text='文字'>
+        <Image />
+      </Grid.Item>
+      <Grid.Item text='文字'>
+        <Image />
+      </Grid.Item>
+      <Grid.Item text='文字'>
+        <Image />
+      </Grid.Item>
+    </Grid>
+  )
+}
