6f11ffd90cfbbbeb279b59099ac43cb717b3b989
Juskinbo <juskinbo@outlook.com>
2023-12-31 00:53:21 +0800

    init MovieComponent

---
+++ src/components/MovieComponent/MovieComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View } from '@tarojs/components'
+
+const MovieComponent = () => {
+  return (
+    <View>
+      <Text>MovieComponent</Text>
+    </View>
+  )
+}
+export default MovieComponent
